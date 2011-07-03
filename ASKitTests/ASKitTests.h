4da305bd188d5674d3ee9a4cd060e41e33a8dc8f
Robert Tully <aersunstrike@googlemail.com>
2011-07-03 11:39:54 +0100

    Initial Commit

---
+++ ASKitTests/ASKitTests.h
@@ -0,0 +1,17 @@
+//
+//  ASKitTests.h
+//  ASKitTests
+//
+//  Created by Robert Tully on 03/07/2011.
+//  Copyright 2011 Azurenode. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ASKitTests : SenTestCase {
+@private
+    
+}
+
+@end
